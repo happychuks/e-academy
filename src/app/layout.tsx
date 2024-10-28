cece0508689dcbc1e086d632fae2df9ac807c5c3
Happy Felix Chukwuma <happychukwuma@gmail.com>
2024-10-28 02:00:32 +0100

    update: updated page metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'E-Academy Auth',
-  description: 'Authentication system for E-Academy',
+  title: 'E-Academy',
+  description: 'E-Academy is an innovative Islamic Edtech platform',
 }
 
 export default function RootLayout({
